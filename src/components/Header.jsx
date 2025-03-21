2ddf69cee84af447503ecb33e7f4b2ee7c9e76e4
svapnnika varma <svapnnikavarma@gmail.com>
2025-03-21 11:36:05 +0530

    changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="App-Header-Row">
       <div>My React Store</div>
-      <div>
+      <div className="App-Header-Links">
         <Link to="home">Home</Link>
         <Link to="cart">Cart</Link>
         <Link to="login">Login</Link>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import "./Register.css"
 export default function Register() {
+  const [details, setDetails] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = () => {
+    const newDetails = {
+      name: name,
+      email: email,
+      password: password
+    };
+    setDetails([...details, newDetails]);
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
+  
   return (
-    <div>
+    <div className='container'>
+      <div>
       <h2>Registration Form</h2>
-      <p><input type="text" placeholder='Enter name'></input></p>
-      <p><input type="text" placeholder='Email address'></input></p>
-      <p><input type="password" placeholder='New password'></input></p>
-      <p><button>Submit</button></p>
+      <p><input type="text" 
+      placeholder='Enter name'
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      ></input></p>
+      <p><input type="text" 
+      placeholder='Email address'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      ></input></p>
+      <p><input type="password" 
+      placeholder='New password'
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      ></input></p>
+      <p>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
       <p>
         <Link to="../login">Already a member? Log In</Link>
       </p>
+      </div>
+      <div>
+        <h3>Details Entered</h3>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+              </tr>
+            </thead>
+            <tbody>
+              {details.length > 0 &&
+              details.map((detail, index) => (
+                  <tr key={index}>
+                      <td>{detail.name}</td>
+                      <td>{detail.email}</td>
+                      <td>{detail.password}</td>
+                  </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
